38798b9170a117e24759bc80a64a1eba4f1ec410
Marcin Januszewski <mjanuszewski@box.com>
2022-10-06 15:00:20 +0200

    this is a test sommit for PCS-8888

--- 4th_file_testing.sh
+++ 4th_file_testing.sh
@@ -1,3 +1,4 @@
 #/! just another test file
 #+
-just chceckin something.
+#just chceckin something.
+# this file will be remove soon in next itteration
---
+++ test_pro_file_no1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# this is test pro file no 1
+# just to make sure that everything is running
